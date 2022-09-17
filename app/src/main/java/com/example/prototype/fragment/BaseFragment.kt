9b92033d37eb1edc6f39267e8fdf22e8c23e71bd
habadong <44386449+habadong@users.noreply.github.com>
2022-09-17 18:35:31 +0900

    마이페이지 추가
    1. 스크롤 뒤 배경 추가
    2. 스크롤뷰 ui 구성
    3. 마이페이지 상단 툴바 생성 (메뉴 진행중) (툴바에 대한 개념및 구현 연습해야 할 듯)

--- app/src/main/java/com/example/prototype/fragment/BaseFragment.kt
+++ app/src/main/java/com/example/prototype/fragment/BaseFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<VB: ViewBinding>: Fragment() {
